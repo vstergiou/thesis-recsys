66ec162cb9ea436b8408251f94dc12ff560341c0
vstergiou <vstergiou01@gmail.com>
2022-05-22 18:29:14 +0300

    added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+
+
+cwd = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'requirements.txt')
+with open(cwd) as f:
+    dependencies = list(map(lambda x: x.replace("\n", ""), f.readlines()))
+
+setup(
+    name='thesis',
+    version='0.1',
+    install_requires=dependencies,
+    packages=['gymrecsys'],
+)
